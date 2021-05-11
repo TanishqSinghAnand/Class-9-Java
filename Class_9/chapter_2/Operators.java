53475ef1e4a24157e066a601a928c732edc7c27d
TanishqSinghAnand <anandtanishqs@gmail.com>
2021-05-11 11:50:27 +0530

    IDK

---
+++ Class_9/chapter_2/Operators.java
@@ -0,0 +1,12 @@
+package Class_9.chapter_2;
+
+public class Operators
+{
+    void tsa()
+    {
+        int a = 7;
+        int b = 2;
+        
+        System.out.println(7%2);
+    }
+}
---
+++ Class_9/chapter_2/ReversePattern.java
@@ -0,0 +1,41 @@
+package Class_9.chapter_2;
+import java.util.Scanner;
+
+public class ReversePattern
+{
+    public static void main(String args[])
+{
+int n, i, j, space = 1;
+System.out.print("Enter the number of rows: ");
+Scanner s = new Scanner(System.in);
+n = s.nextInt();
+space = n - 1;
+for (j = 1; j<= n; j++)
+{
+for (i = 1; i<= space; i++)
+{
+System.out.print(" ");
+}
+space--;
+for (i = 1; i <= 2 * j - 1; i++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+space = 1;
+for (j = 1; j<= n - 1; j++)
+{
+for (i = 1; i<= space; i++)
+{
+System.out.print(" ");
+}
+space++;
+for (i = 1; i<= 2 * (n - j) - 1; i++)
+{
+System.out.print("*");
+}
+System.out.println("");
+}
+}
+}
